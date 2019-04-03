cc356bfaeeadb78820d0f73f7002604f9b3a7201
elainemwang <32891543+elainemwang@users.noreply.github.com>
2019-04-03 08:19:51 -0700

    blackjack Elaine 4/3

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -12,20 +12,39 @@ import java.util.Scanner;
 public class BlackJack
 {
 	//add in Player instance variable
+        private Player player;
 	//add in Dealer instance variable
-
+        private Dealer dealer;
+        
 	public BlackJack()
 	{
-
-
+            player = new Player();
+            dealer = new Dealer();
+            dealer.shuffle();
 	}
 
 	public void playGame()
 	{
-		Scanner keyboard = new Scanner(System.in);
-		char choice = 0;
-
-
+            // deal the player two cards
+     
+            player.addCardToHand(dealer.deal());
+            player.addCardToHand(dealer.deal());
+            // deal the dealer two cards
+            dealer.addCardToHand(dealer.deal());
+            dealer.addCardToHand(dealer.deal());
+            
+            System.out.println("Current " + player);
+            if(player.getHandValue()>=21){
+                
+            }
+            System.out.println("Do you want to hit? [Y/N]");
+            Scanner keyboard = new Scanner(System.in);
+            char choice = keyboard.next().charAt(0);
+            if(choice == 'Y'||choice == 'y'){
+                player.addCardToHand(dealer.deal());
+            }
+            
+           
 
 
 
